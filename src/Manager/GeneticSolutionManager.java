365b475054d098299bc600cf98a78b87bbd24add
FranckParis <franckysnowp@gmail.com>
2017-05-18 14:13:21 +0200

    Genetic population fixed

--- src/Manager/GeneticSolutionManager.java
+++ src/Manager/GeneticSolutionManager.java
@@ -1,9 +1,7 @@
 package Manager;
 
-import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader;
 import object.Solution;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -51,13 +49,6 @@ public class GeneticSolutionManager {
         if(random.nextFloat() <= crossingProba){
             int crossingPos = random.nextInt(inputSolutions.get(0).getSize()-1);
 
-            //Debug displays
-            /*System.out.println("\n");
-            System.out.println("CrossingPos = " + crossingPos);
-            System.out.println("Input A : " + inputSolutions.get(0).getState());
-            System.out.println("Input B : " + inputSolutions.get(1).getState());
-            System.out.println("---------------------------");*/
-
             //First input solution
             for(int i = 0; i<=inputSolutions.get(0).getSize()-1; i++) {
                 if(i <= crossingPos){
@@ -86,29 +77,21 @@ public class GeneticSolutionManager {
             secondSol.addAll(geneSolution21);
             secondSol.addAll(geneSolution12);
 
-            //System.out.println("Output A' : " + firstSol);
-            //System.out.println("Output B' : " + secondSol);
-
             outputSolutions.add(new Solution(firstSol));
             outputSolutions.add(new Solution(secondSol));
-            //System.out.println(outputSolutions);
             return outputSolutions;
         }
 
         else{
-            //System.out.println("Crossing not happening");
             return inputSolutions;
         }
     }
 
     public void mutation (Solution solution, float mutationProba){
         if(random.nextFloat() <= mutationProba) {
-            //System.out.println("---------------------------");
-            //System.out.println("Mutation happening on : " + solution.getState());
             int mutationPos = random.nextInt(solution.getSize()-1);
             int mutationValue = 1 + random.nextInt(solution.getSize());
             solution.getState().set(mutationPos, mutationValue);
-            //System.out.println("Mutated solution : " + solution.getState());
         }
     }
 
@@ -150,26 +133,22 @@ public class GeneticSolutionManager {
         }
 
         //Selection in list
-        int cpt = 1;
+        int cpt;
         int cptDimension = 1;
 
         Solution selectedSolution = probabilizedSolutions.get(random.nextInt(probabilizedSolutions.size()));
         initialAdd.add(selectedSolution);
         outputSolutions.add(initialAdd);
 
-        while(cptDimension < initialPopSize/2){
-            //System.out.println(cptDimension);
+        while(cptDimension < initialPopSize){
             ArrayList<Solution> couple = new ArrayList<>();
             cpt = 0;
 
             while(cpt < reprodSize){
-                //System.out.println("********  CPT = "+ cpt);
                 selectedSolution = probabilizedSolutions.get(random.nextInt(probabilizedSolutions.size()));
-                //System.out.println("Trying solution : " + selectedSolution.getState());
                 boolean addable = true;
 
                 for (Solution sol : couple) {
-                    //System.out.println("Comparing to output sol : " + sol.getState());
                     int cptCommon = 0;
                     for(int i=0; i<sol.getState().size(); i++){
                         if(sol.getState().get(i) == selectedSolution.getState().get(i)){
@@ -183,23 +162,11 @@ public class GeneticSolutionManager {
 
                 if(addable){
                     couple.add(selectedSolution);
-                    //System.out.println("Addable solution : " + selectedSolution.getState() + "\n");
                     cpt++;
-                }
-                else{
-                    //System.out.println("Non addable solution \n");
                 }
             }
             outputSolutions.add(couple);
             cptDimension++;
-        }
-
-        //System.out.println("\n Output solutions after reproduction : ");
-        for (ArrayList<Solution> couple : outputSolutions) {
-            for(Solution sol : couple){
-                //System.out.println(sol.getState() + " | Fitness : "+ (this.dimension*(this.dimension-1) - sol.getNbConflicts()));
-            }
-            //System.out.println("-------------------------------");
         }
 
         return outputSolutions;
@@ -213,13 +180,7 @@ public class GeneticSolutionManager {
         ArrayList<Solution> collect = new ArrayList();
         Solution bestSolution = new Solution(this.dimension);
 
-        /*System.out.println("\n");
-        System.out.println("ReprodSize : " +  reprodSize);
-        System.out.println("InitialPopSize : " + initialPopSize);*/
-
         if(initialPopSize > 1){
-
-            //System.out.println("----------------------- Iteration for size : " + initialPopSize);
 
             //Reproduction
             progessList = reproduction(inputSolutions, reprodSize, initialPopSize);
@@ -238,23 +199,10 @@ public class GeneticSolutionManager {
 
             for (ArrayList<Solution> couple : outputSolutions) {
                 for (Solution sol : couple) {
-                    //System.out.println("Output Solution : " + sol.getState() + " | Fitness : " + (dimension*(dimension-1) - sol.getNbConflicts()));
                     collect.add(sol);
                 }
-                //System.out.println("----------------------------------------");
             }
-
-            /*System.out.println("\n Collect :");
-            for (Solution sol : collect) {
-                System.out.println("Output Solution : " + sol.getState() + " | Fitness : " + (dimension*(dimension-1) - sol.getNbConflicts()));
-            }*/
-
-            if((int)(initialPopSize/2) == 2){
-                geneticResolution(collect, crossingProba, mutationProba, 1, 2);
-            }
-            else{
-                geneticResolution(collect, crossingProba, mutationProba, reprodSize, initialPopSize/2);
-            }
+            geneticResolution(collect, crossingProba, mutationProba, reprodSize, initialPopSize-1);
         }
         if(!collect.isEmpty()){
             bestSolution = findBestSolution(collect);
